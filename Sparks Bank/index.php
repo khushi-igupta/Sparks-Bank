c084508be467b1718a16822c526c508d61d5fafe
khushi-igupta <106496121+khushi-igupta@users.noreply.github.com>
2022-07-21 06:33:08 +0530

    initial commit

---
+++ Sparks Bank/index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Banking System</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <style type="text/css">
+    .logo-text{
+	  font-size:50px;
+	  padding-left: 30%;
+      color: white;
+      padding-top: 20px;
+	  
+    }
+    .h1{font-size:70px;
+    padding-bottom:20px;
+  margin-bottom: 20px;}
+    
+    .main-img{
+      width:100%;
+      margin: 0;
+    }
+    .main-text{
+      text-align: center;
+      font-size: 40px;
+    }
+    .p2{
+      font-size: 30px;
+    }
+    .button1 {
+      background-color: #0b090a;
+      color: white;
+      padding: 15px 32px;
+      text-align: center;
+      text-decoration: none;
+      
+      font-size: 20px;
+      margin: 4px 2px;
+      border-radius: 60px;
+    }
+	.button2 {
+      background-color: #0b090a;
+      border: none;
+      color: white;
+      padding: 15px 32px;
+      text-align: center;
+      text-decoration: none;
+     
+      font-size: 20px;
+      margin: 10px 10px;
+      border-radius: 60px;
+    }
+    
+    .button1:hover{
+      background-color: #5352ed;
+      text-decoration: none;
+      color: #0b090a;
+    }
+    .button2:hover{
+      background-color: #5352ed;
+      text-decoration: none;
+      color: #0b090a;
+    }
+      
+    
+	.view-btn2{
+      text-align:center ;
+    }
+    body{background-color:#f9fcf1;
+    text-align:center;}
+	.mo{
+		 background-color:#f9fcf1; 
+		   padding: 50px 50px;
+		   margin: 10% 20%; 
+	}
+  .
+  .footer p{
+    font-family: sans-serif;
+    
+  }
+  .mainimg{align: center;
+  radius:100%;
+padding-top:40px;
+padding-bottom:40px;}
+
+    </style>
+
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <div class="logo"><img src=".//newimages\images/menu.png" height="100px" width="130px"></div>
+  <p class="logo-text">Sparks Bank</p>
+  </nav>
+  <div class="mo" >
+  <div class="art">
+        <h1>Save Your Money With Bank Sparks</h1>
+        <p>     </p>
+        <p>       </p>
+        <div class="mainimg">
+        <img src="https://source.unsplash.com/700x350/?finance,bank" class="circular">
+        </div>  
+        <h2 id="customer">Customer Details</h2>
+        <pre><b><i>
+            "Improving to provide the best Services!"<br>
+            "We value your time and privacy!"</i></b><br><br><br></pre>
+    <div class="details"><img src=".//newimages\images/customers.png"" height="160px" width="200px"><br><br>
+     
+    
+ 
+ <div class="view-btn2">
+     
+  </div>
+  <br></br>
+  <div class="view-btn2">
+     <a href="viewusers.php" class="button1">Customer Details</a>
+  </div>
+  <br></br>
+  <div class="art">
+    <h2 id="history">Transaction History </h2>
+    <pre><b><i>
+        "Click to check your past transactions."</i></b><br><br><br>
+    </pre>
+    <div class="history"><img src=".//newimages\images/history.png"" height="160px" width="200px"><br><br>
+  <div class="view-btn2">
+     <a href="transactionDetails.php" class="button2">Transaction History</a>
+  </div>
+
+<div></br></br></br></br></br></br></div>
+  </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  
+  
+  
+   <footer class="footer1" align="center"> <br>
+    <div  style="height: 60%">
+    <p><h6>&copy; Khushi Gupta|2022|</h6></p>
+ 
+    </div>
+  </footer>
+  
+  
+  
+  </body>
+</html>
